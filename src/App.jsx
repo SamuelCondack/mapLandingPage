3cac8f953765ec68362dcdf0fa253e382cd3a57d
unknown <samuelcondackxz@gmail.com>
2024-02-05 11:21:34 -0300

    feat: add Statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './Global.css'
 import Header from './components/Header/Header'
-import styles from "./App.module.css"
 import Pics from './components/Pics/Pics'
+import styles from "./App.module.css"
 
 function App() {
 
@@ -9,6 +9,28 @@ function App() {
     <>
       <Header />
       <Pics />
+      <div className={styles.statisticsContainer}>
+        <div className={styles.statisticsItem}>
+          <h2 className={styles.number}>600</h2>
+          <p className={styles.quantity}>Milhões de visitas</p>
+          <p className={styles.description}>registradas por mês nos principais marketplaces do Brasil.</p>
+        </div>
+        <div className={styles.statisticsItem}>
+          <h2 className={styles.number}>R$169,6</h2>
+          <p className={styles.quantity}>Bilhões</p>
+          <p className={styles.description}>de reais faturados no varejo online em 2022.</p>
+        </div>
+        <div className={styles.statisticsItem}>
+          <h2 className={styles.number}>61%</h2>
+          <p className={styles.quantity}>Dos Brasileiros</p>
+          <p className={styles.description}>preferem realizar compras online ao invés de se dirigirem até as lojas físicas.</p>
+        </div>
+        <div className={styles.statisticsItem}>
+          <h2 className={styles.number}>85%</h2>
+          <p className={styles.quantity}>Dos Consumidores</p>
+          <p className={styles.description}>brasileiros utilizam marketplaces como principal plataforma de compras online.</p>
+        </div>
+      </div>
     </>
   )
 }
