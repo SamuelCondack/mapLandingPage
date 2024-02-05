708a9993cb8069631df46cb2bd168c7f1f519146
unknown <samuelcondackxz@gmail.com>
2024-02-05 10:45:55 -0300

    feat: add second content

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './Global.css'
 import Header from './components/Header/Header'
 import firstContent from '../src/assets/firstContent.webp'
 import styles from "./App.module.css"
+import secondContent from '../src/assets/secondContent.webp'
 
 function App() {
 
   return (
     <>
       <Header />
-      <img src={firstContent} className={styles.firstContent} alt="firstContent" />
+      <div className={styles.contentsDiv}><img src={firstContent} className={styles.firstContent} alt="firstContent" /></div>
+      <div className={styles.contentsDiv}><img src={secondContent} className={styles.firstContent} alt="secondContent" />img</div>
     </>
   )
 }
