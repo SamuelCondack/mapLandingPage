762de4b47b5875e04d4eb40f2c3c123985b50ca1
unknown <samuelcondackxz@gmail.com>
2024-02-05 10:37:17 -0300

    feat: add first content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './Global.css'
 import Header from './components/Header/Header'
+import firstContent from '../src/assets/firstContent.webp'
+import styles from "./App.module.css"
 
 function App() {
 
   return (
     <>
       <Header />
+      <img src={firstContent} className={styles.firstContent} alt="firstContent" />
     </>
   )
 }
