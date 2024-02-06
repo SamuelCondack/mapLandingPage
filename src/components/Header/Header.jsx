1436dc3c8cb71fca5d0f7b6bfb2f1475ac659414
unknown <samuelcondackxz@gmail.com>
2024-02-06 11:41:30 -0300

    feat: add transition on header links while on hover

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,17 @@ function Header() {
 
     return (
         <>
-            <div className={styles.headerContainer}>
-                <a href="/"><img src={mapLogo} alt="logo" className={styles.headerLogo} /></a>
-                <ul className={styles.ulLinks}>
-                    <li><a href="home" className={styles.links}>Home</a></li>
-                    <li><a href="quem somos" className={styles.links}>Quem Somos?</a></li>
-                    <li><a href="serviços" className={styles.links}>Serviços</a></li>
-                    <li><a href="produtos" className={styles.links}>Produtos</a></li>
-                    <li><a href="parceiros" className={styles.links}>Parceiros</a></li>
-                    <li><a href="contato" className={styles.links}>Contato</a></li>
-                </ul>
-            </div>
+        <div className={styles.headerContainer}>
+            <a href="/"><img src={mapLogo} alt="logo" className={styles.headerLogo} /></a>
+            <ul className={styles.ulLinks}>
+                <li><a href="home" className={styles.links}>Home</a></li>
+                <li><a href="quem somos" className={styles.links}>Quem Somos?</a></li>
+                <li><a href="serviços" className={styles.links}>Serviços</a></li>
+                <li><a href="produtos" className={styles.links}>Produtos</a></li>
+                <li><a href="parceiros" className={styles.links}>Parceiros</a></li>
+                <li><a href="contato" className={styles.links}>Contato</a></li>
+            </ul>
+        </div>
         </>
     )
 }
